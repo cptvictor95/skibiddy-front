8961d6cdc1b24a8572470e7bd22ce470195edc28
cptvictor95 <cpt.victor@hotmail.com>
2021-05-01 04:50:53 -0300

    move routes from app to routing file

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,18 @@
 import React from "react";
 // Chakra
 import { ChakraProvider, CSSReset } from "@chakra-ui/react";
-// React Router
-import { BrowserRouter, Switch, Route } from "react-router-dom";
 // Font
 import "@fontsource/montserrat";
 // Theme
 import theme from "./utils/theme";
-// Pages
-import Home from "./pages/Home";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import Contact from "./pages/Contact";
-import Features from "./pages/Features";
+
+import Router from "./routing/Router";
 
 const App: React.FC = () => {
   return (
     <ChakraProvider theme={theme}>
       <CSSReset />
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/signin">
-            <SignIn />
-          </Route>
-          <Route path="/signup">
-            <SignUp />
-          </Route>
-          <Route path="/features">
-            <Features />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <Router />
     </ChakraProvider>
   );
 };
---
+++ src/routing/Router.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+// Auth Provider
+import { AuthProvider } from "../provider/authProvider";
+// React Router Dom
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+// Pages
+import Contact from "../pages/Contact";
+import Features from "../pages/Features";
+import Feed from "../pages/Feed";
+import Home from "../pages/Home";
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
+
+const Router: React.FC = () => {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/signin">
+            <SignIn />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/features">
+            <Features />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/feed">
+            <Feed />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+};
+
+export default Router;
