01a8d4c10cd7f86c6d0e7991226d6a461f2229da
cptvictor95 <cpt.victor@hotmail.com>
2021-05-06 14:29:58 -0300

    fix contants url

--- src/contants.tsx
+++ src/contants.tsx
@@ -1,2 +1,2 @@
 export const baseUrlDev = "http://localhost:3002";
-export const baseUrlProd = "https://skibiddy-backend.herokuapp.com/";
+export const baseUrlProd = "https://skibiddy-backend.herokuapp.com";
